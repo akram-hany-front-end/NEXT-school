97b166f30b06b4c7d108184a9eafed8e9ce7874d
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-07-09 16:16:13 +0300

    navbar+dashboardlayout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import "../globals.css";
+import Image from "next/image";
+import Menu from "../../components/Menu";
+import Navbar from "@/components/Navbar";
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/*LEFT*/}
+      <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 "
+        >
+          <Image width={32} height={32} src="/logo.png" alt="logo" />
+          <span className="hidden lg:block">My-School</span>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/*RIGHT*/}
+      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll ">
+      <Navbar/>
+      {children}
+      </div>
+    </div>
+  );
+}
