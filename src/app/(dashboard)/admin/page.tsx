8602c10b9e6df1a514878d840bff3d40d8e5470d
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-07-10 04:20:45 +0300

    recharts-installed/user-card

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,22 @@
-const AdminPage = () => {
-    return (
-        <div className="">AdminPage</div>
-    )
-}
+import UserCard from "@/components/UserCard";
 
-export default AdminPage
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3"> 
+           {/* USER CARDS */}
+       <div className="flex gap-4 bg-akram-cyan flex-wrap justify-between">
+        <UserCard type="student"/>
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="staff"/>
+      </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default AdminPage;
