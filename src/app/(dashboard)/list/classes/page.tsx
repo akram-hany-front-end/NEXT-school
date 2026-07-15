6d8d270c269497949d1b66f91ec2256e7f558b38
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-07-15 17:26:02 +0300

    list pages finished

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -1,86 +1,59 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, teachersData } from "@/lib/data";
+import { classesData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Teacher = {
+type Class = {
   id: number;
-  teacherId: string;
   name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
+  capacity: string;
+  grade: number;
+  supervisor: string;
 };
 
 const columns = [
   {
-    header: "Info",
-    accessor: "Info",
+    header: "Class Name",
+    accessor: "name",
   },
   {
-    header: "Teacher ID",
-    accessor: "teacherId",
+    header: "Capacity ",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+   },
+  {
+    header: "Grede ",
+    accessor: "grade",
     className: "hidden md:table-cell",
   },
   {
-    header: "Subjects",
-    accessor: "subjects",
+    header: "Supervisor ",
+    accessor: "supervisor",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Classes",
-    accessor: "classes",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Phone",
-    accessor: "phone",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
-  },
+
   {
     header: "Actions",
     accessor: "action",
   },
 ];
-const StudentListPage = () => {
-  const renderRow = (item: Teacher) => (
+const ClassListPage = () => {
+  const renderRow = (item: Class) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-akram-purple-light"
     >
-      <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-akram-cyan">
-              <Image src="/view.png" alt="" width={16} height={16} />
+              <Image src="/edit.png" alt="" width={16} height={16} />
             </button>
           </Link>
           {role === "admin" && (
@@ -98,7 +71,7 @@ const StudentListPage = () => {
       {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hiddden md:block text-lg font-semibold ">
-          All Teachers
+          All Classes
         </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
@@ -118,7 +91,7 @@ const StudentListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
       {/* PAGINATION */}
       <div className="">
         <Pagination />
@@ -127,4 +100,4 @@ const StudentListPage = () => {
   );
 };
 
-export default StudentListPage;
+export default ClassListPage;
